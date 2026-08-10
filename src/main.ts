ef61d6fb6fcb5bf2090c7838b279b830d09dc96c
Amr Mohamed <amrmohamed.m.reda@gmail.com>
2026-08-11 00:10:29 +0300

    chore: initialize NestJS project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`);
+  });
+}
+
+bootstrap();
