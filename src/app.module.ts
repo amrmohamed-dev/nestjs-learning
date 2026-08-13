7e9f9b430aba833e89b1b0f50b4cc52356a19307
Amr Mohamed <amrmohamed.m.reda@gmail.com>
2026-08-13 15:49:13 +0300

    feat: add PostgreSQL database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeModule } from './modules/coffees/coffee.module';
 
 @Module({
-  imports: [CoffeeModule],
+  imports: [
+    // Configuration
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // DataBase
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    // Application Modules
+    CoffeeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
